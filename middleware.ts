48a1fac3ffe2247b0a96dba8fef92649441ae251
kaxiix <85871821+kaxiix@users.noreply.github.com>
2024-07-12 17:07:17 +0400

    depreciated

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
+
  
-export default clerkMiddleware({
+export default authMiddleware({
   publicRoutes: [
     '/',
     '/events/:id',
